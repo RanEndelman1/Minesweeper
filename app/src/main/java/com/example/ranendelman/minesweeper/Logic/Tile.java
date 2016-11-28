d5257e7911391f9334c7a1420b29ddded5060a4b
RanEndelman1 <endelmanran@gmail.com>
2016-11-28 12:37:12 +0200

    Start the project

---
+++ app/src/main/java/com/example/ranendelman/minesweeper/Logic/Tile.java
@@ -0,0 +1,8 @@
+package com.example.ranendelman.minesweeper.Logic;
+
+/**
+ * Created by RanEndelman on 26/11/2016.
+ */
+
+public class Tile {
+}
---
+++ app/src/main/java/com/example/ranendelman/minesweeper/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.ranendelman.minesweeper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setupStartButton();
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.begginer:
+                if (checked)
+                    // Beginner level chosen
+                    break;
+            case R.id.medium:
+                if (checked)
+                    // Medium level chosen
+                    break;
+            case R.id.hard:
+                if (checked)
+                    // Hard level chosen
+                    break;
+        }
+    }
+
+    protected void setupStartButton() {
+        ImageButton b = (ImageButton) findViewById(R.id.imageButton3);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+}
